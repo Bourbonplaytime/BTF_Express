e5b07a4aa16d650d45ffec799eac40e32387c181
bourbs <jharvey0010@kctcs.edu>
2021-03-15 14:30:33 -0700

    Changes to the schema reflecting the intention to have twitter handle be the index

--- modules/Users.js
+++ modules/Users.js
@@ -3,9 +3,9 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/btf_profile');
 
 var userSchema = new mongoose.Schema({
-	userID: {type: Number, index: true, required: true, unique: true},
-	username: {type: String, index: true, required: true, unique: true},
-	tweet: { type: String, required: true },
+	userID: {type: Number, required: true, unique: true},
+	username: {type: String, required: true},
+	tweet: { type: String, required: true, unique: true, index: true},
   insta: {type:String},
   fb: {type: String},
   lnk: {type: String},
